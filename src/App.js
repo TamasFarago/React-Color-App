c19c70051eac245f87d962af478d8e99e5ee6412
Tamas Farago <tamas@Tamass-MacBook-Pro.local>
2020-08-23 23:42:12 +0200

    Display colors in SingleColorPalette

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Palette from "./Palette"
 import seedColors from "./seedColors"
 import { generatePalette } from "./ColorHelpers"
 import PaletteList from "./PaletteList"
+import SingleColorPalette from "./SingleColorPalette"
 
 export default class App extends Component {
   findPalette(id){
@@ -14,6 +15,23 @@ export default class App extends Component {
   render() {
     return (
       <Switch>
+
+        <Route 
+        exact 
+        path="/palette/:paletteId/:colorId" 
+        render={routeProps => (
+        <SingleColorPalette 
+          colorId={routeProps.match.params.colorId}
+          palette={generatePalette(
+            this.findPalette(routeProps.match.params.paletteId)
+      )}
+      />
+      )}
+      />  
+
+
+
+
       <Route 
       exact 
       path="/" 
@@ -21,20 +39,20 @@ export default class App extends Component {
       <PaletteList palettes={seedColors} {...routeProps}/>
       )}
       />
-      <Route exact path="/palette/:id" 
-      render={routeProps => (
-        <Palette 
-          palette={generatePalette(
-            this.findPalette(routeProps.match.params.id)
-      )}
-      />
-      )}
-      />
 
+      <Route 
+      exact 
+      path="/palette/:id" 
+      render={routeProps => (
+        <Palette palette={generatePalette(
+          this.findPalette(routeProps.match.params.id)
+        )}
+   
+      
+      />
+      )}
+      />
       </Switch>
-      // <div>
-      //   <Palette palette={generatePalette(seedColors[4])} />
-      // </div>
     )
   }
 }
